78dce1337a5d1fff0949edb3b9e7ff2822534c20
dfbowden <6887766+dfbowden@users.noreply.github.com>
2017-12-01 17:29:07 -0700

    Fixed Q-Value function

--- FinalProject/QLearnAgent.py
+++ FinalProject/QLearnAgent.py
@@ -24,7 +24,6 @@ class QAgent(object):
         self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         self.targets=numpy.zeros(1).reshape(1,1)
         self.states=numpy.zeros((1,5)).reshape(1,5)
-        self.ttest=0
 
     def choose_action(self, state, trial):
         self.explore=max(self.MIN_EXPLORE_RATE, min(1, 1.0 - math.log10((trial+1)/25)))
@@ -44,10 +43,20 @@ class QAgent(object):
         self.learn=max(self.MIN_LEARNING_RATE, min(0.5, 1.0 - math.log10((trial+1)/25)))
         q0 = self.model.predict(numpy.append(new_state,[0]).reshape(1,5))
         q1 = self.model.predict(numpy.append(new_state,[1]).reshape(1,5))
-        qTarget = self.learn*(reward + self.DISCOUNT_FACTOR*max(q0,q1))
-        #self.model.fit(numpy.append(old_state,[action]).reshape(1,5),numpy.array([qTarget]).reshape(1,1), epochs=50, batch_size=1, verbose=0)
-        self.targets=numpy.append(self.targets, numpy.array([qTarget]).reshape(1,1), axis=0)
-        self.states=numpy.append(self.states, numpy.append(old_state,[action]).reshape(1,5), axis=0)
-        if self.ttest != trial:
-            self.model.fit(self.states,self.targets,epochs=1,batch_size=1, verbose=0)
-            self.ttest = trial
+        oldQ = self.model.predict(numpy.append(old_state,[action]).reshape(1,5))
+        qTarget = (1 - self.learn)*oldQ + self.learn*(reward + self.DISCOUNT_FACTOR*max(q0,q1))
+        
+        # Use all data to train NN
+        if trial == 0:
+            print("PING1")
+            self.targets=numpy.array([qTarget]).reshape(1,1)
+            self.states=numpy.append(old_state,[action]).reshape(1,5)
+        else:
+            self.targets=numpy.append(self.targets, numpy.array([qTarget]).reshape(1,1), axis=0)
+            self.states=numpy.append(self.states, numpy.append(old_state,[action]).reshape(1,5), axis=0)
+            
+        # Use only last data point to train NN
+        #self.targets=numpy.array([qTarget]).reshape(1,1)
+        #self.states=numpy.append(old_state,[action]).reshape(1,5)
+        
+        self.model.fit(self.states,self.targets,epochs=1,batch_size=1, verbose=0)
